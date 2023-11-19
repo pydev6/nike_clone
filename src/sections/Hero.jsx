c20894e666848c9e3c481783eb6c2bbea271b178
pydev6 <devpy6@gmail.com>
2023-11-19 11:41:21 -0600

    deleted docs folder

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { arrowRight } from '../assets/icons';
-import { bigShoe1 } from '../assets/images';
-import Button from '../components/Button';
-import ShoeCard from '../components/ShoeCard';
+
 import { shoes, statistics } from '../constants';
+import { Button, ShoeCard } from '../components';
+import { bigShoe1 } from '../assets/images';
+import { arrowRight } from '../assets/icons';
 
 const Hero = () => {
   const [bigShoeImg, setbigShoeImg] = useState(bigShoe1);
